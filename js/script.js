e18252245d711f47bafcf9dc47a7f0b7c2c64695
FrancescaLoria <francesca.loria90@hotmail.it>
2023-03-09 16:25:32 +0100

    milestone - 1

--- js/script.js
+++ js/script.js
@@ -1,14 +1,31 @@
 // MILESTONE 1:
 // Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente due input e un bottone (non stilizzati), realizziamo le specifiche scritte sopra. La risposta finale (o output) sarà anch’essa da scrivere in console.
 
-// RECUPERO I CHILOMETRI PERCORSI 0DALL'UTENTE E LA SUA ETA'
-const kilometresInput = document.querySelector(".kilometres-input");
-
-const ageInput = document.querySelector(".age-input");
+const priceForKm = 0.21;
 
 // METTO LA PAGINA IN ASCOLTO DEL "CLICK" SUL BOTTONE
 const buttonInput = document.querySelector(".submit-button");
 buttonInput.addEventListener("click", function () {
-  console.log("Valore KM", kilometresInput.value);
-  console.log("Valore AGE", ageInput.value);
+  // RECUPERO I CHILOMETRI PERCORSI 0DALL'UTENTE E LA SUA ETA'
+  const kilometresInput = parseInt(
+    document.querySelector(".kilometres-input").value
+  );
+
+  const ageInput = parseInt(document.querySelector(".age-input").value);
+
+  // SULLE BASI DELLA PRIMA DOMANDA CALCOLIAMO IL PREZZO INTERO DEL BIGLIETTO
+  const totalPrice = kilometresInput * priceForKm;
+  // SULLA BASE DELL'ETA' DEL NOSTRO UTENTE CALCOLIAMO LO SCONTO
+  let discount = 0;
+  if (ageInput < 18) {
+    discount = 20;
+  } else if (ageInput > 65) {
+    discount = 40;
+  }
+  console.log("sconto", discount);
+
+  console.log("total price", totalPrice);
+
+  const finalPrice = totalPrice - totalPrice * (discount / 100);
+  console.log(finalPrice.toFixed(2));
 });
